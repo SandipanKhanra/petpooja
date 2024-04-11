0f4a26bfe12e6da7c31c39124f39ef0d10d9ae35
Sandipan Khanra <sandipankhanra@gmail.com>
2024-04-11 17:00:40 +0530

    Folder structure changed

---
+++ src/components/Meals/Meals.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import MealItem from "./MealItem";
+
+const Meals = () => {
+  const [fetchedMeals, setFetchedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      const meals = await response.json();
+      setFetchedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+  return (
+    <ul id="meals">
+      {fetchedMeals.map((meal) => (
+        <MealItem {...meal} key={meal.id} />
+      ))}
+    </ul>
+  );
+};
+
+export default Meals;
